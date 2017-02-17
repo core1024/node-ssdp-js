3c5b44d2db71ccda1912c4f63ab4f6c3274e7e54
Brandon Lehmann <blehmann@velocity.org>
2017-02-17 12:19:56 -0500

    resolved lack of service.js && added ability to supply a queryAuthenticator as an option

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
   module.exports = {
     Server: require('./lib/server'),
     Client: require('./lib/client'),
-    Core: require('./lib/core'),
-    Service: require('./lib/service')
+    Core: require('./lib/core')
   }
 })()
